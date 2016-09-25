5ee3a44e09ab757fd623f4fde41044605cfacc77
nora-yauricaza <nora.yauricaza@gmail.com>
2016-09-25 17:59:04 -0500

    version 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-windows.addEventListener("load"; function(){
-	var texto =document.gerElementById().value;
-	
-});
 
-windows.onkeypress = function(){
+function validar() {
+  var nombre = document.getElementById("nombre").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var expresion = /\w+@\w+\.+[a-z]/;
 
+  if (nombre === "") {
+    alert("El campo nombre esta vacia");
+    return false;
+  }else if (email === ""){
+    alert("El campo email esta vacia");
+    return false;
+  }else if (email.length>30){
+    alert("Email es muy largo")
+    return false;
+  }else if(password.length<6){
+    alert("La contraseña solo deben tener máximo 6 caracteres");
+    return false;
+  }else if(!expresion.test(email)){
+    alert("Email no es válido");
+    return false;
+  }
 }
 
