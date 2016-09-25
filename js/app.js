1f02d8e11e07ad1bd0bda0f60d64b98f02a0aac8
smiling girl.. sonrie   siempre  en la   vida <smiling girl.. sonrie   siempre  en la   vida>
2016-09-25 16:33:15 -0500

    evento drag & drog

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-windows.addEventListener("load"; function(){
-	var texto =document.gerElementById().value;
+
+	function allowDrop(ev) {
+    	ev.preventDefault();
+	}
+
+	function drag(ev) {
+    	ev.dataTransfer.setData("text", ev.target.id);
+	}
+
+	function drop(ev) {
+	    ev.preventDefault();
+	    var data = ev.dataTransfer.getData("text");
+	    ev.target.appendChild(document.getElementById(data));
+	}
 	
-});
 
-windows.onkeypress = function(){
 
-}
+
 
