aae99eaa2a862bbebffae8d84d2d3eae97c4d924
Clic Art Creaciones Publicitarias <Clic Art Creaciones Publicitarias>
2016-09-26 00:02:02 -0500

    agregando divs

--- js/app_trabajo.js
+++ js/app_trabajo.js
@@ -1,23 +1,7 @@
-window.addEventListener("load",function(){
-  var papel=document.getElementById("papel");
-  papel.addEventListener("click",function(e){
-    var evt=window.evt || e;
-    document.getElementById("div2").style.left=evt.clientX+ "px";
-    document.getElementById("div2").style.top=evt.clientY + "px";
-  });
-});
 
 
-  function allowDrop(ev) {
-      ev.preventDefault();
-  }
-
-  function drag(ev) {
-      ev.dataTransfer.setData("text", ev.target.id);
-  }
-
-  function drop(ev) {
-      ev.preventDefault();
-      var data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(data));
-  }
+function addDiv() {
+  var a = document.createElement('div');
+  document.getElementsByTagName('section')[0].appendChild(a);
+  
+}
