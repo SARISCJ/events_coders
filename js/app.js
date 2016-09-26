e623c1d0d0032983b8f6283e6910659e4f038d4f
Clic Art Creaciones Publicitarias <Clic Art Creaciones Publicitarias>
2016-09-26 09:02:51 -0500

    cambios antes de entrega

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var element = document.querySelector(".form-signup");
+element.addEventListener("click", function(event) {
+  event.preventDefault();
+  validateForm();
+
+  function validateForm() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+      if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
+        var span = document.createElement("span");
+        var referencia= document.getElementById("nombre");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debe ingresar su nombre");
+        span.appendChild(texto);
+        return false;
+      } if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))) {
+        var span = document.createElement("span");
+        var referencia= document.getElementById("email");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Verifique su email");
+        span.appendChild(texto);
+        return false;
+      } if( password == null || password.length <= 6 || password == "password" || password == 123456 || password == 098754 || /^\s+$/.test(password) ) {
+        var span = document.createElement("span");
+        var referencia= document.getElementById("password");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Ingresa una contraseña válida");
+        span.appendChild(texto);
+        return false;
+      }
+    } 
+  }
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+### Proyecto Events Coders
+
+## Idea:
+
+Somos un grupo de chicas coders, que no entendiamos bien la funcion que cumplen los eventos y a quien dirigirlos.
+Ya sea creando , moviendo, eliminando, etc, un elemento de nuestra hoja de trabajo.
+
+Ya que por concepto :
+
+```javascript
+element.addEventListener("click", function(event) {
+  event.preventDefault();
+  validateForm();
+```
+
+lo que hace es compilar uno encima de otro aunque no se ve en nuestro html.
+
+Por eso nacio la idea de hacer como un frame Work para coders, donde puedes ir agregando los elementos que vas a usar en tu hoja de trabajo y poderlos mover, asi sabes a quien afecta tu evento.
